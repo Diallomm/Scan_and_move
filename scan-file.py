49458e01b371bda4ffc72f0e699b720777a061b1
Mamadou diallo <misterdiallo1@gmail.com>
2018-04-24 16:53:23 +0000

    first finish Lol

--- scan-file.py
+++ scan-file.py
@@ -1,21 +1,14 @@
 """
-This script allow you to scan your news files with clamav
+This script allow automatc you to scan your news files with clamav and move file in respective directory
 
-permet de connaitre l'extension d'un fichier
-from os.path import splitext
-file_name,extension = splitext('/home/lancaster/Downloads/a.ppt')
 
-deplacer un fichier en Python
-import shutil
-shutil.move("source","Dest")
-
-lister les fichiers dans un dossier
-import glob
-glob.glob("*")
 """
 
 import os
 import tkinter
+import glob
+import shutil
+from os.path import splitext
 
 #Affichage
 mainapp=tkinter.Tk()
@@ -36,15 +29,50 @@ def det_virus(val):
 
 #fonction qui gére les rangements
 def rangements():
-        image = [".jpg", ".png", ".jpeg", ".gif", ".bmp"]
+        image = [".jpg", ".png", ".jpeg", ".gif", ".bmp", ".svg"]
         pdf = [".pdf"]
         word = [".odt", ".doc", ".docx"]
         text = [".txt", ".md"]
         presentation = [".odp", ".pptx", ".ppt", ".ppsx"]
-        compressed = [".zip", ".rar", ".deb", ".tar.xz", ".tar.gz", ".tar.bz2", ".gz", ".tgz"]
+        compressed = [".zip", ".rar", ".deb", ".tar.xz", ".tar.gz", ".tar.bz2", ".gz", ".tgz",".ace","."]
         music = [".mp3", ".wav"]
         video = [".mkv", ".mp4", ".avi", ".wmv", ".mov", ".flv", "webm"]
         torrent = [".torrent"]
+        windows = [".exe",".msi"]
+        programmation = [".py", ".sh", ".csv", ".jar", ".java", ".ino", ".bin",".vcf",".c",".class",".cpp",".css","."]
+
+        liste = glob.glob("*")
+        liste_extension=[]
+
+        for i in liste:
+            file_name,extension=splitext(i)
+            liste_extension.append(extension)
+
+        liste_extension=set(liste_extension)
+
+        for j in liste_extension:
+            if j in image:
+                os.system("mv *{0} /home/misterpi/Images".format(j))
+            elif j in pdf:
+                os.system("mv *{0} /home/misterpi/Documents/pdf".format(j))
+            elif j in word:
+                os.system("mv *{0} /home/misterpi/Documents/odt-docx".format(j))
+            elif j in text:
+                os.system("mv *{0} /home/misterpi/Documents/texte".format(j))
+            elif j in presentation:
+                os.system("mv *{0} /home/misterpi/Documents/presentation".format(j))
+            elif j in compressed:
+                os.system("mv *{0} /home/misterpi/Compressed".format(j))
+            elif j in music:
+                os.system("mv *{0} /home/misterpi/Vidéos".format(j))
+            elif j in video:
+                os.system("mv *{0} /home/misterpi/Musique".format(j))
+            elif j in torrent:
+                os.system("mv *{0} /home/misterpi/Téléchargements/torrent".format(j))
+            elif j in windows:
+                os.system("mv *{0} /home/misterpi/Téléchargements/Autre/exe".format(j))
+            elif j in programmation:
+                os.system("mv *{0} /home/misterpi/Téléchargements/test".format(j))
 
 
 
@@ -53,16 +81,18 @@ os.chdir("/home/{0}/Téléchargements".format(user))
 scan=os.popen("clamscan --move=virus | grep Infected").read()
 nb_virus=det_virus(scan)
 if nb_virus == 0:
-    #print("VIRUS PAS TROUVER")
     app=tkinter.Label(mainapp,text=("VIRUS PAS TROUVER"))
     app.pack()
 elif nb_virus == 1:
-    #print("VIRUS TROUVER \n consulter le dossier virus pour voir les copies")
     app=tkinter.Label(mainapp,text=("VIRUS TROUVER \n consulter le dossier virus pour voir les copies"))
     app.pack()
 elif nb_virus == 2:
     app=tkinter.Label(mainapp,text=("Une erreur (s) s'est produite."))
     app.pack()
-    #print("Une erreur (s) s'est produite.")
+
+
+rangements()
+fini=tkinter.Label(mainapp,text=("vos fichiers sont étaient déplacer avec success"))
+fini.pack()
 
 mainapp.mainloop()
