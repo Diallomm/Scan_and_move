ef7e533e3e7f08d6fa3bd6a1d80c14dedc7550c4
Mamadou diallo <misterdiallo1@gmail.com>
2018-04-27 04:00:47 +0000

    add extension and check clamav(antivirus)

--- check_system_and_install.py
+++ check_system_and_install.py
@@ -1,22 +1,159 @@
-import os, time
+import os, time, os.path, subprocess
+user=os.getlogin()
+os.chdir("/home/{0}".format(user))
+
+# this function check if you have necessery folder to move all file in Downloads to respective folder
+def check_folder():
+    """check if you have respective folder """
+
+    Saisie_folder=input("Do you want to check if you have good folder to move respective file ? Y|N")
+    if Saisie_folder in ["Y","y","yes","YES"]:
+        # make Downloads folders
+        if os.path.exists("/home/{0}/Downloads".format(user)):
+            print("Downloads exist ✔️")
+        else:
+            print("Wait to created Downloads folder")
+            os.makedirs("Downloads")
+
+        # make image folder
+        if os.path.exists("/home/{0}/Images".format(user)):
+            print("Images exist ✔️")
+        else:
+            print("Wait to created Images folder")
+            os.makedirs("Images")
+
+        # make Documents folder
+        if os.path.exists("/home/{0}/Documents".format(user)):
+            print("Documents exist ✔️")
+        else:
+            print("Wait to created Documents folder")
+            os.makedirs("Documents")
+
+        # make PDF folder for (.PDF)
+        if os.path.exists("/home/{0}/Documents/PDF".format(user)):
+            print("PDF ✔️")
+        else:
+            print("Wait to created PDF folder")
+            os.makedirs("Documents/PDF")
+
+        #make Writer folder for (.odt,.docx)
+        if os.path.exists("/home/{0}/Documents/Writer".format(user)):
+            print("Writer exist ✔️")
+        else:
+            print("Wait to created Writer folder")
+            os.makedirs("Documents/Writer")
+
+        #make Text folder for (.txt )
+        if os.path.exists("/home/{0}/Documents/Text".format(user)):
+            print("Text exist ✔️")
+        else:
+            print("Wait to created Text folder")
+            os.makedirs("Documents/Text")
+
+        #make impress and powerpoint folder for (.pptx, .odp )
+        if os.path.exists("/home/{0}/Documents/Presentation".format(user)):
+            print("Presentation exist ✔️")
+        else:
+            print("Wait to created Presentation folder")
+            os.makedirs("Documents/Presentation")
+
+        #make tableur folder for (.xls, .xlsx )
+        if os.path.exists("/home/{0}/Documents/Tableur".format(user)):
+            print("Tableur exist ✔️")
+        else:
+            print("Wait to created Tableur folder")
+            os.makedirs("Documents/Tableur")
+
+        #make compressed folder for (.zip, .tar.bz2, .deb )
+        if os.path.exists("/home/{0}/Compressed".format(user)):
+            print("Compressed exist ✔️")
+        else:
+            print("Wait to created Compressed folder")
+            os.makedirs("Compressed")
+
+        #make Music folder for (.mp3, .ogg, .opus )
+        if os.path.exists("/home/{0}/Music".format(user)):
+            print("Music exist ✔️")
+        else:
+            print("Wait to created Music folder")
+            os.makedirs("Music")
+
+        #make Movie folder for (.mp4, .mkv )
+        if os.path.exists("/home/{0}/Movie".format(user)):
+            print("Movie exist ✔️")
+        else:
+            print("Wait to created Movie folder")
+            os.makedirs("Movie")
+
+        #make torrent folder for (.torrent )
+        if os.path.exists("/home/{0}/Downloads/Torrent".format(user)):
+            print("Torrent exist ✔️")
+        else:
+            print("Wait to created Torrent folder")
+            os.makedirs("Downloads/Torrent")
+
+        #make windows folder for (.exe, .msi, .bat,... )
+        if os.path.exists("/home/{0}/Downloads/Windows".format(user)):
+            print("Windows exist ✔️")
+        else:
+            print("Wait to created Windows folder")
+            os.makedirs("Downloads/Windows")
+
+        #make Code file folder for (.html, .jav, .pl )
+        if os.path.exists("/home/{0}/Downloads/Code_file".format(user)):
+            print("Code_file exist ✔️")
+        else:
+            print("Wait to created Code_file folder")
+            os.makedirs("Downloads/Code_file")
+
+        #make font folder for (.mp4, .mkv )
+        if os.path.exists("/home/{0}/Downloads/Font".format(user)):
+            print("Font exist ✔️")
+        else:
+            print("Wait to created Font folder")
+            os.makedirs("Downloads/Font")
+
+        #make image os folder for (.iso, .img )
+        if os.path.exists("/home/{0}/Downloads/Os".format(user)):
+            print("Os exist ✔️")
+        else:
+            print("Wait to created Os folder")
+            os.makedirs("Downloads/Os")
+
+
+#This function allow you to test if you have clamav (Antivirus)
+def check_clamscan():
+    soft= subprocess.call(['which','clamscan'])
+    if soft == 0:
+        print("clamav is installed ✔️✔️✔️")
+    else:
+        print("A Antivirus clamav is not installed\n")
+        rep=input("Do yo want to install clamav? Y/N  ")
+        if rep in ["Y","y","yes","YES"]:
+            os.system("sudo apt-get install clamav")
+        else :
+            print("Ok! Your files will not scan")
+
 
 """check if you have tkinter """
 
-print ("Hello! let me to check if your system are ready \nmerci de me laisser vérifier si votre système est pret")
+print ("Hello! let me to check if your system are ready")
 try:
     import tkinter
-    print("Super! All is Good")
 except ImportError:
     print("I' sorry you don't have tkinter package\n")
-    Saisie=input("do you want to install tkinter Y/N ?")
+    Saisie=input("do you want to install tkinter Y/N ?  ")
     if Saisie in ["Y","y","yes","YES"]:
         os.system("sudo apt-get install python3-tk")
+        check_folder()
     else:
         print("OK! Good bye")
         time.sleep(1)
         exit()
+else:
+    print("Super! All is Good ✔️✔️✔️")
+    check_folder()
 
-"""check if you have respective folder """
-Saisie_folder=input("Do you want to check if you have good folder to move respective file ?")
-if Saisie_folder in ["Y","y","yes","YES"]:
-    
+""" check if you have Antivirus clamav""""
+check_clamscan()
+print("Super! All is Good ✔️✔️✔️")
--- scan-file.py
+++ scan-file.py
@@ -1,22 +1,22 @@
 """
 This script allow automatc you to scan your news files with clamav and move file in respective directory
 
-#auteur:Mamadou diallo
-#Mail: misterdiallo1@gmail.com
+#Auteur:Mamadou diallo
+#Mail:misterdiallo1@gmail.com
 
 """
-
+#import library
 import os
 import tkinter
 import glob
 import shutil
 from os.path import splitext
 
-#Affichage
+#printing information with tkinter
 mainapp=tkinter.Tk()
 mainapp.title("Alerte")
 
-#fonction permettant de sorti le nombre de virus
+#this function allow you to know if after scanning with clamscan you have virus or not
 def det_virus(val):
     chaine=val.split(" ")
     chaine_list=chaine[2]
@@ -29,7 +29,10 @@ def det_virus(val):
     finish=int(result)
     return finish
 
-#fonction qui gére les rangements
+#this function who allow you to move your file in respective folder
+# image file in folder image, Music file in Music folder ....
+
+#if you have another extension files, you can add here, on the list
 def rangements():
         image = [".jpg", ".png", ".jpeg", ".gif", ".bmp", ".svg", ".drw", ".dwg",".dxf",".eps",".ico",".psd",".tif"]
         pdf = [".pdf"]
@@ -37,13 +40,14 @@ def rangements():
         text = [".txt", ".md",".rtf"]
         presentation = [".odp", ".pptx", ".ppt", ".ppsx",".pps",".ppt",".pub"]
         tableur=[".xls",".xlsx",".ods"]
-        compressed = [".zip", ".rar", ".deb", ".tar.xz", ".tar.gz", ".tar.bz2", ".gz", ".tgz",".ace","."]
-        music = [".mp3", ".wav",".wmv"]
+        compressed = [".zip", ".rar", ".deb", ".tar.xz", ".tar.gz", ".tar.bz2", ".gz", ".tgz",".ace",".rmp"]
+        music = [".mp3", ".wav",".wmv",".opus",".m4a",".ogg"]
         video = [".mkv", ".mp4", ".avi", ".wmv", ".mov", ".flv", "webm", ".mpg",".qt"]
         torrent = [".torrent"]
         windows = [".exe",".msi"]
         programmation = [".py", ".sh", ".csv", ".jar", ".java",".jav", ".ino", ".bin",".vcf",".c",".class",".cpp",".css",".dll",".h",".html",".php",".pl",".sql"]
-        police=[".ttf"]
+        font=[".ttf"]
+        os=[".iso",".img"]
 
         liste = glob.glob("*")
         liste_extension=[]
@@ -54,49 +58,55 @@ def rangements():
 
         liste_extension=set(liste_extension)
 
+# move files in respective folders
         for j in liste_extension:
             if j in image:
-                os.system("mv *{0} /home/misterpi/Images".format(j))
+                os.system("mv *{0} /home/{1}/Images".format(j,user))
             elif j in pdf:
-                os.system("mv *{0} /home/misterpi/Documents/pdf".format(j))
+                os.system("mv *{0} /home/{1}/Documents/PDF".format(j,user))
             elif j in word:
-                os.system("mv *{0} /home/misterpi/Documents/odt-docx".format(j))
+                os.system("mv *{0} /home{1}/Documents/Writer".format(j,user))
             elif j in text:
-                os.system("mv *{0} /home/misterpi/Documents/texte".format(j))
+                os.system("mv *{0} /home/{1}/Documents/Text".format(j,user))
             elif j in presentation:
-                os.system("mv *{0} /home/misterpi/Documents/presentation".format(j))
+                os.system("mv *{0} /home/{1}/Documents/Presentation".format(j,user))
             elif j in compressed:
-                os.system("mv *{0} /home/misterpi/Compressed".format(j))
+                os.system("mv *{0} /home/{1}/Compressed".format(j,user))
             elif j in music:
-                os.system("mv *{0} /home/misterpi/Vidéos".format(j))
+                os.system("mv *{0} /home/{1}/Movie".format(j,user))
             elif j in video:
-                os.system("mv *{0} /home/misterpi/Musique".format(j))
+                os.system("mv *{0} /home/{1}/Music".format(j,user))
             elif j in torrent:
-                os.system("mv *{0} /home/misterpi/Téléchargements/torrent".format(j))
+                os.system("mv *{0} /home/{1}/Downloads/torrent".format(j,user))
             elif j in windows:
-                os.system("mv *{0} /home/misterpi/Téléchargements/Autre/exe".format(j))
+                os.system("mv *{0} /home/{1}/Downloads/Windows".format(j,user))
             elif j in programmation:
-                os.system("mv *{0} /home/misterpi/Téléchargements/test".format(j))
+                os.system("mv *{0} /home/{1}/Downloads/Code_file".format(j,user))
+            elif j in font:
+                os.system("mv *{0} /home/{1}/Downloads/Font"..format(j,user))
+            elif j in os:
+                os.system("mv *{0} /home/{1}/Downloads/Os"..format(j,user))
 
 
-
+#get name of user
 user=os.getlogin()
-os.chdir("/home/{0}/Téléchargements".format(user))
+os.chdir("/home/{0}/Downloads".format(user))
+#use clamav to scan Downloads folder
 scan=os.popen("clamscan --move=virus | grep Infected").read()
 nb_virus=det_virus(scan)
+#check and print informations with tkinter
 if nb_virus == 0:
-    app=tkinter.Label(mainapp,text=("VIRUS PAS TROUVER"))
+    app=tkinter.Label(mainapp,text=("VIRUS NOT FOUND ✔️"))
     app.pack()
 elif nb_virus == 1:
-    app=tkinter.Label(mainapp,text=("VIRUS TROUVER \n consulter le dossier virus pour voir les copies"))
+    app=tkinter.Label(mainapp,text=("VIRUS FOUND ❌\n consult the virus folder to see the copies"))
     app.pack()
 elif nb_virus == 2:
-    app=tkinter.Label(mainapp,text=("Une erreur (s) s'est produite."))
+    app=tkinter.Label(mainapp,text=("An error (s) occurred. ❌"))
     app.pack()
 
-
 rangements()
-fini=tkinter.Label(mainapp,text=("vos fichiers sont étaient déplacer avec success"))
-fini.pack()
+end=tkinter.Label(mainapp,text=("Your files are were moving with success! ✔️"))
+end.pack()
 
 mainapp.mainloop()
